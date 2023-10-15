ee86b2bbba45b190c8411e13abb30decd4ffa5cd
Patrick Ngatia <patrickngatia14@gmail.com>
2023-10-16 00:46:02 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+form.addEventListener('submit',(e)=>{
+e.preventDefault();
+const inputItem =document.querySelector('form > input#new-task-description').value;
+ 
+const task = document.querySelector('#tasks');
+ const li = document.createElement('li');
+ li.innerText=inputItem
+ task.appendChild(li);
+
+});
+
 });
